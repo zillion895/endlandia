0e29c5ddcbd89b7b7fff00077fbcc42c3870de4f
zillion895 <zillion895@gmail.com>
2026-07-22 15:37:07 -0600

    dev textures and changes to main menu

--- endlandia/mainMenu.gd
+++ endlandia/mainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_start():
 	#check for save files
-	var current_level_path = "res://world1.tscn"
+	var current_level_path = "res://scenes/world_1.tscn"
 	# load scene
 	#
 	get_tree().change_scene_to_file(current_level_path)
